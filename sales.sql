558fa7c486418687fdad472537a212fd3cfe38fd
Kirtipanchal <34736268+Kirtipanchal@users.noreply.github.com>
2019-05-06 11:18:30 +1200

    Add files via upload

---
+++ sales.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [CustomerId] INT NOT NULL, 
+    [ProductId] INT NOT NULL, 
+    [StoreId] INT NOT NULL, 
+    [DateSold] DATE NOT NULL
+)
